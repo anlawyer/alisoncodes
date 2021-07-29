7b3912f5cca278b070ae7cb183fe28dca84802fa
anlawyer <anlawyer@berkeley.edu>
2021-07-29 17:07:54 -0400

    Re-structure components and delete unused ones

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,46 @@
 import React, { Component } from "react";
-import Layout from "../components/Layout";
+
+import Head from "../components/Head";
+import MainContent from "../components/MainContent";
+
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: midnightblue;
+    background-color: mintcream;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    font-size: 1.1rem;
+    line-height: 1.5rem;
+    margin: 1rem;
+    a {
+      color: orangered;
+    }
+    a:hover {
+      background-color: peachpuff;
+    }
+  }
+`;
+
+const MainContainer = styled.div`
+  margin: 1rem auto;
+  max-width: 800px;
+  display: flex;
+  flex-direction: column;
+  min-height: 90vh;
+`;
 
 class App extends Component {
   render() {
     return (
-      <Layout />
+      <>
+        <GlobalStyle />
+        <Head />
+        <MainContainer>
+          <MainContent />
+        </MainContainer>
+      </>
     );
   }
 }
