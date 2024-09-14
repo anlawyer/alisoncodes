6a007c4b6f8bb5132b81b397eea90553e45ffbfe
anlawyer <anlawyer@berkeley.edu>
2024-09-14 17:54:48 -0400

    Updating content

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -22,22 +22,37 @@ const MainContent = () => (
     <h1>Hi, I'm Alison.</h1>
 
     <p>
-      I'm a frontend web developer living in Washington, DC.{" "}
+      I'm a student at Columbia University in New York City.{" "}
       <span role="img" aria-label="sunny emoji">
         ☀️
       </span>
     </p>
 
-    <SectionTitle>Coding by day, and sometimes by night</SectionTitle>
+    <SectionTitle>Former software developer, aspiring ecologist</SectionTitle>
     <p>
-      I currently work remotely for{" "}
-      <Link href="https://bentoforbusiness.com">Bento for Business</Link>, a
-      FinTech startup based in San Francisco/Chicago, where I build the web and
-      mobile interfaces of the business expense management platform with React
-      and React Native.
+      I'm currently a student at Columbia University completing the Ecology,
+      Evolution and Environmental Biology post-baccalaureate prorgram. I'm
+      hoping to go into a Masters or PhD program focusing on an area of
+      environmental science after finishing the program at Columbia.
+    </p>
+    <SectionTitle>Background</SectionTitle>
+    <p>
+      After I graduated from{" "}
+      <Link href="https://www.berkeley.edu">UC Berkeley</Link> with a BA in{" "}
+      <Link href="https://cogsci.berkeley.edu">Cognitive Science</Link> in 2017,
+      I completed two software engineering bootcamps before being hired by Bento
+      for Business, a FinTech startup, in 2018. After the company was acquired
+      by U.S. Bank, I worked for the bank for 3 years where I was promoted to
+      Software Engineering Manager and helped grow our engineering organization
+      5x the size it was when we were a startup.
     </p>
     <p>
-      While I lived in Oakland, CA, I volunteered for{" "}
+      I lived in Washington, DC and Alexandria, VA for a few years where I
+      joined the Weed Warriors program from the National Parks Service. We met
+      bi-monthly to remove invasive plant species from the local National Parks.
+    </p>
+    <p>
+      I also lived in Oakland, CA for a while, and while there I volunteered for{" "}
       <Link href="https://openoakland.org">OpenOakland</Link> (a Code for
       America <Link href="https://brigade.codeforamerica.org">brigade</Link>
       ). I was on the steering committee for the year of 2019 where I lead the
@@ -54,27 +69,18 @@ const MainContent = () => (
       <Link href="https://github.com/anlawyer">See my code on GitHub</Link>
     </LinkContainer>
 
-    <SectionTitle>Education</SectionTitle>
-    <p>
-      After I graduated from{" "}
-      <Link href="https://www.berkeley.edu">UC Berkeley</Link> with a BA in{" "}
-      <Link href="https://cogsci.berkeley.edu">Cognitive Science</Link> in 2017,
-      I completed two software engineering bootcamps before being hired by Bento
-      in 2018.
-    </p>
-
     <LinkContainer>
       <Link href="https://www.linkedin.com/in/alisonlawyer/">
-        More details on LinkedIn
+        Find more details on LinkedIn
       </Link>
     </LinkContainer>
 
     <SectionTitle>Et cetera</SectionTitle>
     <p>
       When I'm not reading a sci-fi book, I like to learn about humans,
-      psychology, design, and civic technology. I also love traveling and
-      spending time outdoors, including hiking, biking, camping, and rock
-      climbing. I have a goal to visit every National Park in the US some day.
+      psychology, design, and science. I also love traveling and spending time
+      outdoors, including hiking, biking, camping, and rock climbing. I have a
+      goal to visit every National Park in the US some day.
     </p>
   </Content>
 );
